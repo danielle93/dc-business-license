0e7cb677d4d59c680f9818683ed81a180876d395
Danielle Roberson <danielle.roberson@icloud.com>
2018-04-12 01:14:33 -0700

    initial

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+// https://cdnjs.cloudflare.com/ajax/libs/sidr/2.2.1/jquery.sidr.min.js
+
+// call Sidr Plugin. Toggle by Default
+$('#sidr-show-hide').sidr();
+
+$('.ui.accordion')
+  .accordion()
+;
+
